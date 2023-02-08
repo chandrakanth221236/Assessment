d201a0ccc611123170d5d5b8f0aee27288e767cb
chandrakanth221236 <32702534+chandrakanth221236@users.noreply.github.com>
2023-02-08 13:19:40 +0530

    Add files via upload

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Chandrakanth_CRUD.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Chandrakanth_CRUD.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Book> Book { get; set; }
+    }
+}
