9729910e62e9d668364402438ccf41a4c7205c0d
chandrakanth221236 <32702534+chandrakanth221236@users.noreply.github.com>
2023-02-08 13:22:26 +0530

    Add files via upload

---
+++ Models/Book.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+
+ namespace Chandrakanth_CRUD.Models
+{
+    public class Book
+    {
+
+        [Key]
+        [Display(Name = "Book Id")]
+        [Required]
+        public int Book_id { get; set; }
+        
+        [Display(Name = "Publisher")]
+        [Required]
+        public string Publisher { get; set; }
+        
+        [Display(Name = "Title Name")]
+        [Required]
+        public string Title  { get; set; }
+        public string AuthorLastName { get; set; }
+        public string AuthorFirstName { get; set; }
+        [Required]
+        public float Price { get; set; }
+        public string Style_Citation { get; set; }
+    }
+}
